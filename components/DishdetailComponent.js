84ad28b7ea173fb115c200414d3624ce75eef5a9
Victor Cruz <vicc23@gmail.com>
2020-10-12 17:47:16 -0500

    Redux Adding Favorites

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -3,13 +3,19 @@ import { Text, View, ScrollView, FlatList } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { postFavorite } from '../redux/ActionCreators';
 
 const mapStateToProps = (state) => {
     return {
         dishes: state.dishes,
         comments: state.comments,
+        favorites: state.favorites
     };
 };
+
+const mapDispatchToProps = (dispatch) => ({
+    postFavorite: (dishId) => dispatch(postFavorite(dishId)),
+});
 
 function RenderDish(props) {
     const dish = props.dish;
@@ -66,15 +72,9 @@ function RenderComments(props) {
 }
 
 class Dishdetail extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            favorites: [],
-        };
-    }
 
     markFavorite(dishId) {
-        this.setState({ favorites: this.state.favorites.concat(dishId) });
+        this.props.postFavorite(dishId);
     }
 
     render() {
@@ -83,7 +83,7 @@ class Dishdetail extends React.Component {
             <ScrollView>
                 <RenderDish
                     dish={this.props.dishes.dishes[+dishId]}
-                    favorite={this.state.favorites.some((el) => el === dishId)}
+                    favorite={this.props.favorites.some((el) => el === dishId)}
                     onPress={() => this.markFavorite(dishId)}
                 />
                 <RenderComments
@@ -95,4 +95,4 @@ class Dishdetail extends React.Component {
         );
     }
 }
-export default connect(mapStateToProps)(Dishdetail);
+export default connect(mapStateToProps, mapDispatchToProps)(Dishdetail);
--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+
 import { dishes } from './dishes';
 import { comments } from './comments';
 import { promotions } from './promotions';
 import { leaders } from './leaders';
+import { favorites } from './favorites';
 
 export const ConfigureStore = () => {
     const store = createStore(
@@ -13,6 +15,7 @@ export const ConfigureStore = () => {
             comments,
             promotions,
             leaders,
+            favorites
         }),
         applyMiddleware(thunk, logger)
     );
